7f76f84a4d7ad78906878851b4fbeffdf64337b2
GCCIS <no@no.com>
2023-04-17 15:45:23 -0400

    0.2
    Framework created

--- PacketCapture.py
+++ PacketCapture.py
@@ -1,4 +1,11 @@
-print('Version 0.1')
-print('Test')
-print('Cooper is cool')
-print('Test2')
+#Packet Filtering
+# Import PCAP Files
+# Filter Out Everything Except "Echo Requests" and "Echo Replies"
+# Place in new Filtered Output files "Node*_filtered.txt"
+#Packet Parsing
+# Read in data from text files
+# Parse Metrics
+#Compute Metrics
+# Compute any additional Metrics
+# Format Metrics in correct format (See "https://mycourses.rit.edu/d2l/le/news/1012453/776947/view")
+# Output Formatted Metrics to "Output_Metrics.csv"
