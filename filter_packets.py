f7dce92cb767b79d55ea4f46fc05756485b26451
cmr9082 <82056332+cmr9082@users.noreply.github.com>
2023-04-25 23:09:55 -0400

    v0.2.4
    Added and Expanded on Framework for filter_packet.py

--- filter_packets.py
+++ filter_packets.py
@@ -1,3 +1,26 @@
-def filter() :
-	print('called filter function in filter_packets.py')
-	
+def listPackets(filename,list) :
+	infile = open(filename, 'r')
+	line = infile.readline()
+	while line:
+		#find a way to identify one packet
+		packet = 'needs to be assigned'
+		#Store packet in List
+		list.append(packet)
+	infile.close()
+def getCheck(filename) :
+	infile = open(filename, 'r')
+	line = infile.readline()
+	while line:
+		line = infile.readline()
+		if (line[0] == ('N')) :
+			line = infile.readline()
+			print(line)
+			#get index at beginning of the line
+
+
+	infile.close()
+#main
+packets = []
+getCheck('Node1.txt')
+listPackets('Node1.txt',packets)
+
