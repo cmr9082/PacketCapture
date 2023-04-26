fc4d9308c7be354703e28d7580270d8d67ac0e1d
cmr9082 <82056332+cmr9082@users.noreply.github.com>
2023-04-26 02:46:12 -0400

    v0.2.5
    List to check against packets is finished. listPackets just needs to intake all the packets.

--- filter_packets.py
+++ filter_packets.py
@@ -1,26 +1,22 @@
 def listPackets(filename,list) :
 	infile = open(filename, 'r')
 	line = infile.readline()
-	while line:
-		#find a way to identify one packet
-		packet = 'needs to be assigned'
-		#Store packet in List
-		list.append(packet)
-	infile.close()
-def getCheck(filename) :
+def getCheck(filename,data) :
 	infile = open(filename, 'r')
 	line = infile.readline()
 	while line:
 		line = infile.readline()
-		if (line[0] == ('N')) :
+		if (line[0:3] == ('No.')) :
 			line = infile.readline()
-			print(line)
 			#get index at beginning of the line
-
-
+			if(line.find('Echo') >= 0) :
+				temp=line.lstrip()
+				check = temp[0:temp.index(' ')]
+				data.append(check)
 	infile.close()
 #main
 packets = []
-getCheck('Node1.txt')
-listPackets('Node1.txt',packets)
-
+checklist = []
+getCheck('Node1.txt', checklist)
+#listPackets('Node1.txt',packets)
+print(checklist)
