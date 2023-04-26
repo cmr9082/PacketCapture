5e1214a6f5988881db08b1cf0a0cd7a14d8d330f
GCCIS <no@no.com>
2023-04-26 16:11:48 -0400

    function looping, need to extract correct data

--- packet_parser.py
+++ packet_parser.py
@@ -1,6 +1,16 @@
-doc = open('Node*_filtered.txt','r')
+import sys
 
 def parse() :
 	print('called parse function in packet_parser.py')
+	metricz = []
+	with open(sys.argv[1], 'r') as doc:
+		for line in doc:
+			if not line.strip():	
+				newline = doc.readline().strip()
+				newline.split(" ")
+				if newline[4] == "ICMP":
+					print(newline[4])
+				else:
+					continue
 
-	
+parse()
