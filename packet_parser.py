6c8f7eafeff9a2e5e4be97d7f5a002437282f347
V.J. Goh <mcg4527@rit.edu>
2023-04-29 13:18:47 -0400

    packet_parser returns list of lists with metrics

--- packet_parser.py
+++ packet_parser.py
@@ -1,16 +1,26 @@
 import sys
 
-def parse() :
-	print('called parse function in packet_parser.py')
-	metricz = []
-	with open(sys.argv[1], 'r') as doc:
-		for line in doc:
-			if not line.strip():	
-				newline = doc.readline().strip()
-				newline.split(" ")
-				if newline[4] == "ICMP":
-					print(newline[4])
-				else:
-					continue
+def parse():
+    print('called parse function in packet_parser.py')
+    metrics = []
+    with open(sys.argv[1], 'r') as doc:
+        for line in doc:
+                newline = doc.readline().strip()
+                parts = newline.split()
+                if len(parts) >= 8 and parts[4] == "ICMP":
+                    time = parts[1]
+                    protocol = parts[4]
+                    length = parts[5]
+                    info = ' '.join(parts[7:])
+                    metrics.append((time, protocol, length, info))
+    return metrics
 
-parse()
+results = parse()
+print(results)
+
+for metrics in results:
+    print('Time:', metrics[0])
+    print('Protocol:', metrics[1])
+    print('Length:', metrics[2])
+    print('Info:', metrics[3])
+    print()
