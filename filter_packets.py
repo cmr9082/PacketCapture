a520529e09db50fbde74b8210f268db4121f978c
cmr9082 <82056332+cmr9082@users.noreply.github.com>
2023-05-01 13:32:46 -0400

    v0.3.0
    Reformatted filter_packet.py, much more efficient.

--- filter_packets.py
+++ filter_packets.py
@@ -1,34 +1,24 @@
-def listPackets(filename,list) :
+def filter(filename,list,filtered) :
 	infile = open(filename, 'r')
 	line = infile.readline()
+	isEcho = False
 	packet = ""
 	while line :
 		if((line[0:3] != ('No.'))) :
 			packet = packet + "\n"+ line
+			if(line.find('Echo') >= 0) :
+				isEcho = True
 			line = infile.readline()
 		elif((line[0:3] == ('No.'))):
 			list.append(packet)
+			if(isEcho) :
+				filtered.append(packet)
 			packet = ""
 			packet = packet + "\n"+ line
 			line = infile.readline()
-	
-
-def getCheck(filename,data) :
-	infile = open(filename, 'r')
-	line = infile.readline()
-	while line:
-		line = infile.readline()
-		if (line[0:3] == ('No.')) :
-			line = infile.readline()
-			#get index at beginning of the line
-			if(line.find('Echo') >= 0) :
-				temp=line.lstrip()
-				check = temp[0:temp.index(' ')]
-				data.append(check)
-	infile.close()
 #main
 packets = []
-checklist = []
-getCheck('Node1.txt', checklist)
-listPackets('Node1.txt',packets)
-
+filtered = []
+filter('Node1.txt',packets,filtered)
+print(packets[1])
+print(filtered[2])
